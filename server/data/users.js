bdd4e2aae8eb131f157acf97c7a16026731784bd
ujasitalia <ujasitalia216@gmail.com>
2023-04-07 08:30:50 -0400

    delete user route

--- server/data/users.js
+++ server/data/users.js
@@ -1,68 +1,83 @@
-const mongoCollections = require('../config/mongoCollections');
+const mongoCollections = require("../config/mongoCollections");
 const users = mongoCollections.users;
-const helpers = require('../helpers');
-const bcrypt = require('bcrypt');
+const helpers = require("../helpers");
+const bcrypt = require("bcrypt");
 const saltRounds = 16;
+const { ObjectId } = require("mongodb");
 
-const signup = async (
-  username, 
-  password
-) => {
+const signup = async (username, password) => {
   username = helpers.usernameHandler(username);
 
   const userCollection = await users();
-  const existing = await userCollection.findOne({username: username});
-  if (existing !== null){
-    throw 'Error: User with this username already exists.'
+  const existing = await userCollection.findOne({ username: username });
+  if (existing !== null) {
+    throw "Error: User with this username already exists.";
   }
 
   password = await bcrypt.hash(helpers.passwordHandler(password), saltRounds);
 
   const newUser = {
     username: username,
-    password: password
-  }
+    password: password,
+  };
   const insertInfo = await userCollection.insertOne(newUser);
   if (!insertInfo.acknowledged || !insertInfo.insertedId)
-    throw 'Error: Could not add user';
-  
-  const createdUser = await userCollection.findOne({_id: insertInfo.insertedId});
+    throw "Error: Could not add user";
+
+  const createdUser = await userCollection.findOne({
+    _id: insertInfo.insertedId,
+  });
   return {
     _id: createdUser._id.toString(),
-    username: createdUser.username
-  }
-}
+    username: createdUser.username,
+  };
+};
 
-const checkUser = async (
-  username, 
-  password
-) => {
+const checkUser = async (username, password) => {
   username = helpers.usernameHandler(username);
   password = helpers.passwordHandler(password);
 
   const userCollection = await users();
-  const existing = await userCollection.findOne({username: username});
-  if (existing === null){
-    throw 'Error: Either the username or password is invalid';
+  const existing = await userCollection.findOne({ username: username });
+  if (existing === null) {
+    throw "Error: Either the username or password is invalid";
   }
 
   let comparison = false;
   try {
     comparison = await bcrypt.compare(password, existing.password);
   } catch (e) {
-    throw 'Error: Internal authentication error';
+    throw "Error: Internal authentication error";
   }
-  if (comparison){
+  if (comparison) {
     return {
       _id: existing._id.toString(),
-      username: existing.username
+      username: existing.username,
     };
   } else {
-    throw 'Error: Either the username or password is invalid';
+    throw "Error: Either the username or password is invalid";
   }
-}
+};
+
+//delete user
+const deleteUser = async (id) => {
+  if (!id)
+    throw {
+      status: 404,
+      error: "The user must be logged in to delete their account",
+    };
+
+  let userCollection = await users();
+  let deletedUserInfo = await userCollection.deleteOne({ _id: ObjectId(id) });
+
+  if (deletedUserInfo.deleteCount === 0)
+    throw { status: 400, error: "Could not delete user" };
+
+  return "successful";
+};
 
 module.exports = {
   signup,
-  checkUser
-}
+  checkUser,
+  deleteUser,
+};
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,72 +1,90 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const data = require('../data');
+const data = require("../data");
 const userData = data.users;
-const {ObjectId} = require('mongodb');
-const helpers = require('../helpers');
+const { ObjectId } = require("mongodb");
+const helpers = require("../helpers");
 
-router
-  .route('/signup')
-  .post(async (req, res) => {
-    if (req.session.user){
-      return res.status(403).json({error: 'Error: A user is already logged in.'});
+router.route("/signup").post(async (req, res) => {
+  if (req.session.user) {
+    return res
+      .status(403)
+      .json({ error: "Error: A user is already logged in." });
+  }
+  let { username, password } = req.body;
+  try {
+    username = helpers.usernameHandler(username);
+    password = helpers.passwordHandler(password);
+  } catch (e) {
+    return res.status(400).json({ error: e });
+  }
+  try {
+    const newUser = await userData.signup(username, password);
+    return res.json(newUser);
+  } catch (e) {
+    if (e === "Error: User with this username already exists.") {
+      return res.status(400).json({ error: e });
+    } else {
+      return res.status(500).json({ error: e });
     }
-    let {username, password} = req.body;
-    try{
-      username = helpers.usernameHandler(username);
-      password = helpers.passwordHandler(password);
-    } catch (e) {
-      return res.status(400).json({error: e});
-    }
-    try{
-      const newUser = await userData.signup(username, password);
-      return res.json(newUser);
-    } catch (e) {
-      if (e === 'Error: User with this username already exists.'){
-        return res.status(400).json({error: e});
-      } else {
-        return res.status(500).json({error: e});
-      }
-    }
-  });
+  }
+});
 
-router
-  .route('/login')
-  .post(async (req, res) => {
-    if (req.session.user){
-      return res.status(403).json({error: 'Error: A user is already logged in.'});
+router.route("/login").post(async (req, res) => {
+  if (req.session.user) {
+    return res
+      .status(403)
+      .json({ error: "Error: A user is already logged in." });
+  }
+  let { username, password } = req.body;
+  try {
+    username = helpers.usernameHandler(username);
+    password = helpers.passwordHandler(password);
+  } catch (e) {
+    return res.status(400).json({ error: e });
+  }
+  try {
+    const user = await userData.checkUser(username, password);
+    req.session.user = {
+      _id: user._id,
+      username: user.username,
+    };
+    return res.json(user);
+  } catch (e) {
+    if (e === "Error: Either the username or password is invalid") {
+      return res.status(403).json({ error: e });
+    } else {
+      return res.status(500).json({ error: e });
     }
-    let {username, password} = req.body;
-    try{
-      username = helpers.usernameHandler(username);
-      password = helpers.passwordHandler(password);
-    } catch (e) {
-      return res.status(400).json({error: e});
-    }
-    try{
-      const user = await userData.checkUser(username, password);
-      req.session.user = {
-        _id: user._id,
-        username: user.username
-      }
-      return res.json(user);
-    } catch (e) {
-      if (e === 'Error: Either the username or password is invalid'){
-        return res.status(403).json({error: e});
-      } else {
-        return res.status(500).json({error: e});
-      }
-    }
-  });
+  }
+});
 
-router
-  .route('/logout')
-  .get(async (req, res) => {
-    if (!req.session.user){
-      return res.status(403).json({error: 'No user is currently signed in.'});
+router.route("/logout").get(async (req, res) => {
+  if (!req.session.user) {
+    return res.status(403).json({ error: "No user is currently signed in." });
+  }
+  req.session.destroy();
+  return res.json({ message: "You have been logged out successfully." });
+});
+
+//delete user
+router.route("/delete").delete(async (req, res) => {
+  let id = req.session.user._id;
+  if (!id) {
+    return res
+      .status(404)
+      .json({ error: "You must be logged in to delete their account" });
+  }
+  try {
+    let deletedUser = await userData.deleteUser(id);
+    if (deletedUser === "successful") {
+      return res.status(200).json({ message: "User Deleted Successfully" });
     }
-    req.session.destroy();
-    return res.json({message: 'You have been logged out successfully.'});
-  });
+  } catch (error) {
+    if (error.status) {
+      return res.status(error.status).json(error.error);
+    }
+  }
+});
 
 module.exports = router;
